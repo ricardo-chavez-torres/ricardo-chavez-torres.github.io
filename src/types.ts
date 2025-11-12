e905f020e5ec2e3326babc7e41f786e3df91ca4f
ricardo-chavez-torres <chaveztorresricardo@gmail.com>
2025-11-12 06:19:56 +0100

    typed TocHeading

--- src/types.ts
+++ src/types.ts
@@ -17,3 +17,12 @@ export type Socials = {
   NAME: string;
   HREF: string;
 }[];
+
+// Table of contents heading type used by the TOC components
+export type TocHeading = {
+  text: string;
+  slug: string;
+  depth: number;
+  // allow nested headings (h2 -> h3)
+  sub: TocHeading[];
+};
